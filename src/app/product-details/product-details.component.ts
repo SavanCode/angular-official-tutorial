81c8ff1933f689706b6c60ababee7c0523a86c2d
SavanCode <ciyexiong@gmail.com>
2022-01-23 18:09:39 +0800

    comment: add comment for product detial

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,11 +9,12 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
+  //这里主要是让页面可以使用router中的内容
   constructor(private route: ActivatedRoute) {}
+  //下面是为了获取router中的信息
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
-
     // Find the product that correspond with the id provided in route.
     this.product = products.find(
       (productItem) => productItem.id === productIdFromRoute
